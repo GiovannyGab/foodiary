bb7d4eea419c417badb389a27f1284b944c11f7b
GiovannyG <giovanny.gabrieldsn@gmail.com>
2025-07-19 10:42:47 -0300

    adding util functions to set up the project

---
+++ src/controllers/SigninController.ts
@@ -0,0 +1,9 @@
+import { access } from "fs";
+import { HttpRequest, HttpResponse } from "../types/Http";
+import { ok } from "../utils/http";
+
+export class SignInController{
+  static async  handle(request:HttpRequest): Promise<HttpResponse> {
+   return ok({accessToken:'ola'})
+  }
+}
